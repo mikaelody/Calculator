30b128383aae384c1d42a7349bfac7c9d7b0de47
mikaelody <odysius123@gmail.com>
2023-03-13 17:40:16 +0700

    menambahkan fungsi pengurangan

--- calculator.cs
+++ calculator.cs
@@ -1,12 +1,18 @@
 class Calculator{
 	static void Main(String[] args){
+		int a = 16;
+		int b = 10;
 		
-	}
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, penambahan(a,b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, penngurangan(a,b));
+		Console.WriteLine("\n Tekan sembarang key untuk keluar");
+		Console.ReadKey();
+		}
 	
 	static int penambahan (int a, int b){
 		return a + b;
 	}
-	static int penambahan (int a, int b){
+	static int penngurangan (int a, int b){
 		return a - b;
 	}
 }
